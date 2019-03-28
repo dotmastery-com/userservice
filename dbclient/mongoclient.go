3158327b9de77b24d388903415733cfbb1df8545
Robert Barr <robertbarr.uk@gmail.com>
2019-03-28 16:33:06 +0000

    changes for running on openshuft

--- dbclient/mongoclient.go
+++ dbclient/mongoclient.go
@@ -2,19 +2,41 @@ package dbclient
 
 import (
 	"UserService/model"
+	"time"
+
 	"github.com/rs/xid"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	MongoDBHosts = "mongodb"
+	AuthDatabase = "sampledb"
+	AuthUserName = "user3G3"
+	AuthPassword = "skxIfr8Ocn2QxU07"
+	TestDatabase = "sampledb"
 )
 
 type MongoClient struct {
 	session *mgo.Session
 }
 
+// Connect to the Mongo database
 func (mc *MongoClient) Connect() {
 
 	var err error
-	mc.session, err = mgo.Dial("localhost")
+
+	// We need this object to establish a session to our MongoDB.
+	mongoDBDialInfo := &mgo.DialInfo{
+		Addrs:    []string{MongoDBHosts},
+		Timeout:  60 * time.Second,
+		Database: AuthDatabase,
+		Username: AuthUserName,
+		Password: AuthPassword,
+	}
+
+	mc.session, err = mgo.DialWithInfo(mongoDBDialInfo)
+
 	if err != nil {
 		panic(err)
 	}
@@ -29,7 +51,7 @@ func (mc *MongoClient) ensureIndex() {
 	session := mc.session.Copy()
 	defer session.Close()
 
-	c := session.DB("store").C("users")
+	c := session.DB(TestDatabase).C("users")
 
 	index := mgo.Index{
 		Key:        []string{"id"},
@@ -49,7 +71,7 @@ func (mc *MongoClient) AuthenticateUser(user model.User) (bool, error) {
 	session := mc.session.Copy()
 	defer session.Close()
 
-	c := session.DB("store").C("users")
+	c := session.DB(TestDatabase).C("users")
 
 	total, error := c.Find(bson.M{"username": user.UserName, "password": user.Password}).Count()
 
@@ -65,7 +87,7 @@ func (mc *MongoClient) UserExists(user model.User) (bool, error) {
 	session := mc.session.Copy()
 	defer session.Close()
 
-	c := session.DB("store").C("users")
+	c := session.DB(TestDatabase).C("users")
 
 	total, error := c.Find(bson.M{"username": user.UserName}).Count()
 
@@ -76,26 +98,24 @@ func (mc *MongoClient) UserExists(user model.User) (bool, error) {
 	return false, error
 }
 
-
 func (mc *MongoClient) SaveUser(user model.User) error {
 
 	session := mc.session.Copy()
 	defer session.Close()
 
 	user.Id = xid.New().String()
-	c := session.DB("store").C("users")
+	c := session.DB(TestDatabase).C("users")
 
 	err := c.Insert(user)
 
 	return err
 }
 
-
-func (mc *MongoClient) GetAllUsers()([]model.User, error) {
+func (mc *MongoClient) GetAllUsers() ([]model.User, error) {
 
 	session := mc.session.Copy()
 
-	c := session.DB("store").C("users")
+	c := session.DB(TestDatabase).C("users")
 
 	var users []model.User
 	error := c.Find(bson.M{}).All(&users)
