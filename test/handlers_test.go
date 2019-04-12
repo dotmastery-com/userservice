0673e69e4c65399ea6fa5b39199feefdf03dede7
Robert Barr <robertbarr.uk@gmail.com>
2019-04-12 14:43:40 +0100

    Added handler test

--- test/handlers_test.go
+++ test/handlers_test.go
@@ -196,3 +196,71 @@ func testAuthUser(t *testing.T, input bool, err error, expected string, errcode 
 			rr.Body.String(), expected)
 	}
 }
+
+func TestRegisterUsersTableTest(t *testing.T) {
+
+	var tests = []struct {
+		userExists      bool
+		userExistsError error
+		saveUserError   error
+		expected        string
+		errcode         int
+	}{
+		//-- new user
+		{false, nil, nil, "", 200},
+
+		//-- user already exists
+		{true, nil, nil, "{message: \"User exists\"}", 403},
+
+		//-- Error while checking if user exists
+		{false, errors.New("DB error"), nil, "{message: \"Database error\"}", 500},
+
+		//-- Error while saving user
+		{false, nil, errors.New("DB error"), "{message: \"Database error\"}", 500},
+	}
+
+	for _, test := range tests {
+		testRegisterUser(t, test.userExists, test.userExistsError, test.saveUserError, test.expected, test.errcode)
+	}
+}
+
+func testRegisterUser(t *testing.T, userExists bool, userExistsError error, saveUserError error, expected string, errcode int) {
+	mockDatabase := new(mock.DBClientMock)
+	service.MyEnv.DB = mockDatabase
+
+	mockDatabase.On("SaveUser").Return(saveUserError)
+	mockDatabase.On("UserExists").Return(userExists, userExistsError)
+
+	user := &model.User{
+		Id:       "TestUser",
+		UserName: "UserName",
+		Password: "Password",
+	}
+
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(user)
+	req, err := http.NewRequest("POST", "/register", buf)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.RegisterUser)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != errcode {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, errcode)
+	}
+
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
